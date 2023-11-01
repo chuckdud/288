1726d09bb7eb4140e64aed4c8bbff7c08a377d62
Dudley <cddudley@iastate.edu>
2023-11-01 13:54:50 -0500

    finished lab 9 and started 10

---
+++ lab10/main.c
@@ -0,0 +1,8 @@
+#include "servo.h"
+
+int main(void)
+{
+    servo_init();
+    servo_move(0);
+    return 0;
+}
--- lab9/main.c
+++ lab9/main.c
@@ -9,9 +9,14 @@ int main(void)
 {
     ping_init();
     lcd_init();
-    trigger_ping();
-    while (edgeCount < 2);
-    int delta  = timeRisingEdge-timeFallingEdge;
-    lcd_printf("rise: %d\nfall: %d\ndelta: %d", timeFallingEdge, timeRisingEdge, delta);
-	return delta;
+    while (1) {
+        trigger_ping();
+        while (edgeCount < 2);
+        edgeCount = 0;
+        int delta  = timeRisingEdge-timeFallingEdge;
+//        lcd_printf("rise: %d\nfall: %d\ndelta: %d", timeFallingEdge, timeRisingEdge, delta);
+        lcd_printf("delta: %d cycles\ndistance: %.2f cm", delta, deltaToDistance(delta));
+        timer_waitMillis(1000);
+    }
+//	return delta;
 }
